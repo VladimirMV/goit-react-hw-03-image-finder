f933fa5ca6c0881182c86e2bb1d0f2f87330019c
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-05-09 12:07:01 +0300

    Ребрендинг с componentDidUpdate

--- src/components/App.js
+++ src/components/App.js
@@ -16,53 +16,58 @@ class App extends Component {
     page: 1,
   };
 
-  handleSubmit = async inputData => {
-    const page = 1;
-    this.setState({ items: [], status: 'idle' });
-    if (inputData.trim() === '') {
-      Notiflix.Notify.info('You cannot search by empty field, try again.');
-      return;
-    } else {
-      try {
-        this.setState({ status: 'pending' });
-        const { totalHits, hits } = await fetchImages(inputData, page);
-        if (hits.length < 1) {
-          this.setState({ status: 'idle' });
-          Notiflix.Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        } else {
-          this.setState(prevState => ({
-            items: [...prevState.items, ...hits],
-            inputData, // Update inputData only on successful submit (on resolved status).
-            totalHits: totalHits,
-            status: 'resolved',
-          }));
+  async componentDidUpdate(_, prevState) {
+    const { inputData, page } = this.state;
+
+    if (
+      this.state.inputData !== prevState.inputData ||
+      page !== prevState.page
+    ) {
+      if (inputData.trim() === '') {
+        Notiflix.Notify.info('You cannot search by empty field, try again.');
+        return;
+      } else {
+        try {
+          const { totalHits, hits } = await fetchImages(inputData, page);
+          if (hits.length < 1) {
+            this.setState({ status: 'idle' });
+            Notiflix.Notify.failure(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
+          } else {
+            this.setState(prevState => ({
+              items: [...prevState.items, ...hits],
+              inputData: inputData,
+              totalHits: totalHits,
+              status: 'resolved',
+            }));
+          }
+        } catch (error) {
+          this.setState({ status: 'rejected' });
         }
-      } catch (error) {
-        this.setState({ status: 'rejected' });
       }
     }
-  };
+  }
 
-  onNextPage = async () => {
-    const { inputData, page } = this.state;
-    this.setState({ status: 'pending' });
-
-    try {
-      const { hits } = await fetchImages(inputData, page + 1);
-      this.setState(prevState => ({
-        items: [...prevState.items, ...hits],
-        page: prevState.page + 1,
-        status: 'resolved',
-      }));
-    } catch (error) {
-      this.setState({ status: 'rejected' });
+  handleSubmit = inputData => {
+    console.log('handleSubmit = ', inputData, this.state.inputData);
+    if (this.state.inputData === inputData) {
+      return;
     }
+    this.setState({
+      inputData: inputData,
+      items: [],
+      status: 'idle',
+      totalHits: 0,
+      page: 1,
+    });
   };
+
+  onNextPage = () => this.setState(({ page }) => ({ page: page + 1 }));
 
   render() {
     const { status, items, totalHits, page } = this.state;
+
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSubmit} />
